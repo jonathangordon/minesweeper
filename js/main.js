74a3d561745098215361eb9146e8966a5a1a30ee
Antonio Giorlando <agiorlando@gmail.com>
2013-06-13 15:09:06 -0700

    Render grid dynamically with click event handler

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 var game = {
 
+  gridSize: 8,
+  mineCount: 10,
+
+
+  // Init the game, change context
   init: function () {
-    console.log($);
+    game.continue();
+  },
+
+  // Begin game
+  continue: function () {
+    this.renderGrid();
+  },
+
+  // Render grid in the DOM
+  renderGrid: function () {
+    for (x = 1; x <= this.gridSize; x++) {
+      // Render rows first
+      var row = $('<div class="row"></div>').appendTo('#board');
+
+      // Render cells with actions
+      for (y = 1; y <= this.gridSize; y++) {
+        var cell = $('<div></div>', {
+          'class': 'cell',
+          'data-x': x,
+          'data-y': y,
+          on: {
+            click: this.cellClicked
+          }
+        }).appendTo(row);
+      }
+    }
+  },
+
+  cellClicked: function () {
+    console.log(this);
   }
 
 };
