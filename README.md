a04a5d82a2a7b687553217ff6af980ed5f0f166d
Jonathan Gordon <jonathan@simpleplea.se>
2013-07-25 17:48:29 -0700

    Clarified arguments in app file and updated README with link to live demo.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ minesweeper
 ===========
 
 Classic Minesweeper in JS/HTML/CSS!
+
+Works on PCs and touch-enabled mobile devices.
+
+Play a demo on [my web site](http://simpleplea.se/minesweeper/ "Play Minesweeper!"). Easy-mode enabled on a 13x13 grid.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 $(function () {
-  var grid = new Game.Grid('#board', 0.2, 8, 12, true, true);
+  var node        = '#board';
+  var probability = 0.25;
+  var columns     = 13;
+  var rows        = columns;
+  var easymode    = true;
+  var hints       = false;
+
+  var grid = new Game.Grid(node, probability, columns, rows, easymode, hints);
 });
